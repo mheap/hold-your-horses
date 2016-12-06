02215a7dd81d23da5df259b0d8202b2174e9c80b
Michael Heap <m@michaelheap.com>
2016-12-06 21:13:53 +0000

    MergePreventer working

--- src/MergePreventer.js
+++ src/MergePreventer.js
@@ -1,30 +1,45 @@
-var __ = function(client, owner, repo, sha){
+var __ = function(client, gh_token, owner, repo, sha){
     this.client = client;
+    this.github_token = gh_token;
     this.owner = owner;
     this.repo = repo;
     this.sha = sha;
+
+    this.githubUrl = "https://api.github.com";
 };
 
 __.prototype.getStatusUrl = function() {
     return '/repos/' + this.owner + '/' + this.repo + '/statuses/' + this.sha;
 };
 
+__.prototype.trigger = function(timeout, pendingCb, successCb){
+    this.setPending(pendingCb);
+    var s = setTimeout(function(){
+        this.setSuccess(successCb)
+    }.bind(this), timeout);
+};
+
 __.prototype.setState = function(state, cb) {
-    this.client.post(this.getStatusUrl(), {"json": true, "body": {
+    this.client.post(this.githubUrl + this.getStatusUrl(), {"json": true, "body": {
         "state": state,
         "context": "Hold Your Horses",
         "description": "Leave it a little while before merging this PR to give others a chance to look at it"
-    }, function(err, httpResponse, body) {
-        return cb(err, httpResponse, body);
-    }});
+    }, "headers": {
+        "User-Agent": "MergePreventer/1.0",
+        "Authorization": "token " + this.github_token
+    }}, function(err, httpResponse, body) {
+        if (typeof cb === "function"){
+            return cb(err, httpResponse, body);
+        }
+    });
 };
 
-__.prototype.setPending = function() {
-    return this.setState("pending");
+__.prototype.setPending = function(cb) {
+    return this.setState("pending", cb);
 };
 
-__.prototype.setSuccess = function() {
-    return this.setState("success");
+__.prototype.setSuccess = function(cb) {
+    return this.setState("success", cb);
 };
 
 module.exports = __;
--- test/MergePreventer_test.js
+++ test/MergePreventer_test.js
@@ -1,22 +1,107 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const request = require("request");
 const MergePreventer = require("../src/MergePreventer");
 
 describe("MergePreventer", function() {
+    beforeEach(function(done){
+        this.clock = sinon.useFakeTimers();
+        done();
+    });
+
+    afterEach(function(done){
+        this.clock.restore();
+        done();
+    });
+
     describe("#getStatusUrl", function() {
         it("should return the correct URL", function() {
             var client = {};
-            var preventer = new MergePreventer(client, "mheap", "hold-your-horses", "shhhaaaa1");
+            var preventer = new MergePreventer(client, "ghTOKEN", "mheap", "hold-your-horses", "shhhaaaa1");
             expect(preventer.getStatusUrl()).to.equal('/repos/mheap/hold-your-horses/statuses/shhhaaaa1');
         });
 
+    });
+
+    describe("#trigger", function() {
+        it("Should call pending, but not success before the timeout", function() {
+            var preventer = new MergePreventer(request, "ghTOKEN",  "mheap", "hold-your-horses", "shhhaaaa1");
+
+            var mock = sinon.mock(preventer);
+            mock.expects("setPending").once();
+            mock.expects("setSuccess").never();
+
+            preventer.trigger(100);
+            this.clock.tick(80);
+
+            mock.verify();
+        });
+
+        it("Should call pending, then success after specified timeout", function() {
+            var preventer = new MergePreventer(request, "ghTOKEN", "mheap", "hold-your-horses", "shhhaaaa1");
+
+            var mock = sinon.mock(preventer);
+            mock.expects("setPending").once();
+            mock.expects("setSuccess").once();
+
+            preventer.trigger(100);
+            this.clock.tick(100);
+
+            mock.verify();
+        });
+    });
+
+    describe("#setPending", function() {
         it("should make a request to the provided client", function() {
-            var client = {
-                "post": sinon.spy()
-            }
-            var preventer = new MergePreventer(client, "mheap", "hold-your-horses", "shhhaaaa1");
+            var mock = sinon.mock(request);
+
+            mock.expects("post").once().withArgs(
+                'https://api.github.com/repos/mheap/hold-your-horses/statuses/shhhaaaa1',
+                {
+                    "json": true,
+                    "body": {
+                        "state": "pending",
+                        "context": "Hold Your Horses",
+                        "description": "Leave it a little while before merging this PR to give others a chance to look at it"
+                    },
+                    "headers": {
+                        "User-Agent": "MergePreventer/1.0",
+                        "Authorization": "token ghTOKEN"
+                    }
+                }
+            );
+
+            var preventer = new MergePreventer(request, "ghTOKEN", "mheap", "hold-your-horses", "shhhaaaa1");
             preventer.setPending();
-            expect(client.post.calledOnce).to.be.true;
+
+            mock.verify();
+        });
+    });
+
+    describe("#setSuccess", function() {
+        it("should make a request to the provided client", function() {
+            var mock = sinon.mock(request);
+
+            mock.expects("post").once().withArgs(
+                'https://api.github.com/repos/mheap/hold-your-horses/statuses/shhhaaaa1',
+                {
+                    "json": true,
+                    "body": {
+                        "state": "success",
+                        "context": "Hold Your Horses",
+                        "description": "Leave it a little while before merging this PR to give others a chance to look at it"
+                    },
+                    "headers": {
+                        "User-Agent": "MergePreventer/1.0",
+                        "Authorization": "token ghTOKEN"
+                    }
+                }
+            );
+
+            var preventer = new MergePreventer(request, "ghTOKEN", "mheap", "hold-your-horses", "shhhaaaa1");
+            preventer.setSuccess();
+
+            mock.verify();
         });
     });
 });
